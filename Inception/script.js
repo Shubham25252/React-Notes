530bdf919dd7f134574019e27e55174245e7b5bb
Shubham <shusb77@gmail.com>
2024-09-12 02:47:40 +0530

    notes

---
+++ Inception/script.js
@@ -0,0 +1,6 @@
+const heading=React.createElement("h1",{
+    id:"title",
+    class:"titles"
+},"Hello this is React!");
+const root=ReactDOM.createRoot(document.querySelector('.text'));
+root.render(heading);
